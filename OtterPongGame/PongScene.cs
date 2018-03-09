7b117028069d9a77282bc34b03a8df8d08ba5dd2
type.h621 <type.h621@gmail.com>
2018-03-09 14:19:32 -0800

    Initial commit

---
+++ OtterPongGame/PongScene.cs
@@ -0,0 +1,29 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtterPongGame
+{
+    class PongScene : Scene
+    {
+        public PongScene()
+        {
+            var testSquare = new Entity(512, 320, Image.CreateRectangle(32, 32, Color.White));
+            var mov = new BasicMovement(1000, 1000, 100);
+            mov.Axis = Global.player.Controller.Axis(Global.Controls.Axis);
+            testSquare.AddComponent(mov);
+
+            Add(testSquare);
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            Draw.Circle(Input.MouseScreenX, Input.MouseScreenY, 7, Color.Gold);
+            //Console.WriteLine("hello hello testing");
+        }
+    }
+}
---
+++ OtterPongGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+using static OtterPongGame.Global;
+
+namespace OtterPongGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game("Otter Pong", 1000, 750);
+
+            Global.player = game.AddSession("Player1");
+
+            Global.player.Controller.AddAxis(Controls.Axis, Axis.CreateWASD());
+            Global.player.Controller.AddButton(Controls.Action);
+            Global.player.Controller.Button(Controls.Action).AddKey(Key.Space);
+
+            game.FirstScene = new PongScene();
+            game.Start();
+        }
+    }
+}
